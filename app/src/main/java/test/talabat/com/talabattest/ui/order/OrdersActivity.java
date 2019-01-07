95cf9f120f1f356d986ba3a6c7fb8027eb6a8465
Mohamed1Fouad <mohamed22fouad@gmail.com>
2019-01-07 12:35:21 +0200

    fix bugs when gps turned off

--- app/src/main/java/test/talabat/com/talabattest/ui/order/OrdersActivity.java
+++ app/src/main/java/test/talabat/com/talabattest/ui/order/OrdersActivity.java
@@ -141,7 +141,7 @@ public class OrdersActivity extends AppCompatActivity implements OrdersView, OnM
     protected void onResume() {
         super.onResume();
         if (Utils.checkLocationPermission(this) && locManager != null)
-            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,
                     0, this);
     }
 
@@ -203,202 +203,232 @@ public class OrdersActivity extends AppCompatActivity implements OrdersView, OnM
 
     public void initLocation() {
         if (Utils.checkLocationPermission(this)) {
+            boolean gps_enabled, network_enabled;
             locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-                    0, this);
-
-            currentLocation = locManager
-                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            gps_enabled = locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            network_enabled = locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
 
-            if (currentLocation == null) {
-                showToast(getString(R.string.error_location));
+            if (gps_enabled) {
+                locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            }
+            if (network_enabled) {
+                locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
 
+//            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,
+//                    0, this);
+
+                currentLocation = locManager
+                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+
+                if (currentLocation == null) {
+                    showToast(getString(R.string.error_location));
+
+                } else {
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 5));
+
+                }
+
+
+            }
+        }
+    }
+
+        //initialize order's info popup
+        public void initInfoWindows () {
+            Button deleteBtn, directionBtn, detailsBtn;
+            final ViewGroup infoWindow;
+            final OnInfoWindowElemTouchListener deleteBtnListener, directionBtnListener, detailsBtnListener;
+
+            mapWrapperLayout.init(this.googleMap, Utils.getPixelsFromDp(this, 50));
+            infoWindow = (ViewGroup) getLayoutInflater().inflate(R.layout.map_marker_info_window, null);
+            deleteBtn = infoWindow.findViewById(R.id.deleteBtn);
+            directionBtn = infoWindow.findViewById(R.id.directionBtn);
+            detailsBtn = infoWindow.findViewById(R.id.detailsBtn);
+
+            deleteBtnListener = new OnInfoWindowElemTouchListener(deleteBtn,
+                    getResources().getDrawable(R.drawable.rounded_button_yellow_shape),
+                    getResources().getDrawable(R.drawable.rounded_button_yellow_shape)) {
+                @Override
+                protected void onClickConfirmed(View v, Marker marker) {
+                    for (Order order : orders) {
+                        if ((Integer) marker.getTag() == order.getOrder_id()) {
+                            orders.remove(order);
+                            break;
+                        }
+                    }
+
+                    markers.remove(marker);
+//                ArrayList<Marker> updatedMarkers = new ArrayList<>(markers);
+                    googleMap.clear();
+                    markers.clear();
+                    //redraw markers after delete selected one
+
+                    for (Order order : orders) {
+                        String latlong[] = order.getUser_location().split(", ");
+                        LatLng loc = new LatLng(Double.valueOf(latlong[0]), Double.valueOf(latlong[1]));
+                        Marker newMarker = googleMap.addMarker(new MarkerOptions().position(loc)
+                                .title(order.getResturant_name()));
+                        newMarker.setTag(order.getOrder_id());
+                        markers.add(newMarker);
+                    }
+
+                    showToast(getString(R.string.order_finished, marker.getTitle()));
+                }
+            };
+
+            directionBtnListener = new OnInfoWindowElemTouchListener(directionBtn,
+                    getResources().getDrawable(R.drawable.rounded_button_yellow_shape),
+                    getResources().getDrawable(R.drawable.rounded_button_yellow_shape)) {
+                @Override
+                protected void onClickConfirmed(View v, Marker marker) {
+                    // Here we can perform some action triggered after clicking the button
+
+                    if (currentLocation != null) {
+                        route(marker.getPosition());
+                        marker.hideInfoWindow();
+                    } else {
+                        showToast(getString(R.string.error_location));
+                    }
+                }
+            };
+
+            detailsBtnListener = new OnInfoWindowElemTouchListener(detailsBtn,
+                    getResources().getDrawable(R.drawable.rounded_button_yellow_shape),
+                    getResources().getDrawable(R.drawable.rounded_button_yellow_shape)) {
+                @Override
+                protected void onClickConfirmed(View v, Marker marker) {
+                    // Here we can perform some action triggered after clicking the button
+                    for (Order order : orders) {
+                        if ((Integer) marker.getTag() == order.getOrder_id()) {
+                            Intent intent = new Intent(OrdersActivity.this, OrderDetailsActivity.class);
+                            intent.putExtra(OrderDetailsActivity.ORDER_KEY, order);
+                            ArrayList<OrderDetails> orderDetails = new ArrayList<>(order.getOrder_details());
+                            intent.putExtra(OrderDetailsActivity.ORDER_DETAILS_KEY, orderDetails);
+
+                            startActivity(intent);
+                            break;
+                        }
+                    }
+                }
+            };
+            deleteBtn.setOnTouchListener(deleteBtnListener);
+            directionBtn.setOnTouchListener(directionBtnListener);
+            detailsBtn.setOnTouchListener(detailsBtnListener);
+
+            this.googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    // Setting up the infoWindow with current's marker info
+                    deleteBtnListener.setMarker(marker);
+                    directionBtnListener.setMarker(marker);
+                    detailsBtnListener.setMarker(marker);
+                    // We must call this to set the current marker and infoWindow references
+                    // to the MapWrapperLayout
+                    mapWrapperLayout.setMarkerWithInfoWindow(marker, infoWindow);
+                    return infoWindow;
+                }
+            });
+        }
+
+
+        public void route (LatLng end){
+            start = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            this.end = end;
+            if (start == null || end == null) {
+                if (start == null) {
+                    showToast("Please choose a starting point.");
+                }
+                if (end == null) {
+                    showToast("Please choose a destination.");
+                }
             } else {
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 5));
-
+                showProgressBar();
+                Routing routing = new Routing.Builder()
+                        .travelMode(AbstractRouting.TravelMode.DRIVING)
+                        .withListener(this)
+                        .alternativeRoutes(true)
+                        .waypoints(start, end)
+                        .key(getString(R.string.google_maps_key))
+                        .build();
+                routing.execute();
             }
-
-
         }
-    }
 
-    //initialize order's info popup
-    public void initInfoWindows() {
-        Button deleteBtn, directionBtn, detailsBtn;
-        final ViewGroup infoWindow;
-        final OnInfoWindowElemTouchListener deleteBtnListener, directionBtnListener, detailsBtnListener;
-
-        mapWrapperLayout.init(this.googleMap, Utils.getPixelsFromDp(this, 50));
-        infoWindow = (ViewGroup) getLayoutInflater().inflate(R.layout.map_marker_info_window, null);
-        deleteBtn = infoWindow.findViewById(R.id.deleteBtn);
-        directionBtn = infoWindow.findViewById(R.id.directionBtn);
-        detailsBtn = infoWindow.findViewById(R.id.detailsBtn);
-
-        deleteBtnListener = new OnInfoWindowElemTouchListener(deleteBtn,
-                getResources().getDrawable(R.drawable.rounded_button_yellow_shape),
-                getResources().getDrawable(R.drawable.rounded_button_yellow_shape)) {
-            @Override
-            protected void onClickConfirmed(View v, Marker marker) {
-                markers.remove(marker);
-                ArrayList<Marker> updatedMarkers = new ArrayList<>(markers);
-                googleMap.clear();
-                markers.clear();
-                //redraw markers after delete selected one
-                for (Order order : orders) {
-                    if ((Integer) marker.getTag() == order.getOrder_id()) {
-                        orders.remove(order);
-                        break;
-                    }
-                }
-                for (Marker marker1 : updatedMarkers) {
-                    Marker newMarker = googleMap.addMarker(new MarkerOptions().position(marker1.getPosition())
-                            .title(marker1.getTitle()));
-                    newMarker.setTag(marker1.getTag());
-                    markers.add(newMarker);
-                }
-
-                showToast(getString(R.string.order_finished, marker.getTitle()));
+        @Override
+        public void onRoutingFailure (RouteException e){
+            hideProgressBar();
+            if (e != null && e.getStatusCode() == "ZERO_RESULTS") {
+                showToast("no direction for This location is so far");
+            } else if (e == null) {
+                showToast("Something went wrong, Try again");
+            } else {
+                showToast("Error: " + e.getMessage());
             }
-        };
+        }
 
-        directionBtnListener = new OnInfoWindowElemTouchListener(directionBtn,
-                getResources().getDrawable(R.drawable.rounded_button_yellow_shape),
-                getResources().getDrawable(R.drawable.rounded_button_yellow_shape)) {
-            @Override
-            protected void onClickConfirmed(View v, Marker marker) {
-                // Here we can perform some action triggered after clicking the button
+        @Override
+        public void onRoutingStart () {
+            googleMap.clear();
+            markers.clear();
+//            for (Marker marker1 : markers) {
+//                googleMap.addMarker(new MarkerOptions().position(marker1.getPosition())
+//                        .title(marker1.getTitle()));
+//            }
 
-                route(marker.getPosition());
-                marker.hideInfoWindow();
+            for (Order order : orders) {
+                String latlong[] = order.getUser_location().split(", ");
+                LatLng loc = new LatLng(Double.valueOf(latlong[0]), Double.valueOf(latlong[1]));
+                Marker newMarker = googleMap.addMarker(new MarkerOptions().position(loc)
+                        .title(order.getResturant_name()));
+                newMarker.setTag(order.getOrder_id());
+                markers.add(newMarker);
             }
-        };
+        }
 
-        detailsBtnListener = new OnInfoWindowElemTouchListener(detailsBtn,
-                getResources().getDrawable(R.drawable.rounded_button_yellow_shape),
-                getResources().getDrawable(R.drawable.rounded_button_yellow_shape)) {
-            @Override
-            protected void onClickConfirmed(View v, Marker marker) {
-                // Here we can perform some action triggered after clicking the button
-                for (Order order : orders) {
-                    if ((Integer)marker.getTag() == order.getOrder_id()) {
-                        Intent intent=new Intent(OrdersActivity.this, OrderDetailsActivity.class);
-                        intent.putExtra(OrderDetailsActivity.ORDER_KEY, order);
-                        ArrayList<OrderDetails> orderDetails=new ArrayList<>(order.getOrder_details());
-                        intent.putExtra(OrderDetailsActivity.ORDER_DETAILS_KEY,orderDetails );
+        @Override
+        public void onRoutingSuccess (ArrayList < Route > route,int shortestRouteIndex){
 
-                        startActivity(intent);
-                        break;
-                    }
+            LatLng start = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            hideProgressBar();
+
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(start, 10));
+
+
+            if (polylines.size() > 0) {
+                for (Polyline poly : polylines) {
+                    poly.remove();
                 }
             }
-        };
-        deleteBtn.setOnTouchListener(deleteBtnListener);
-        directionBtn.setOnTouchListener(directionBtnListener);
-        detailsBtn.setOnTouchListener(detailsBtnListener);
 
-        this.googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(Marker marker) {
-                return null;
+            polylines = new ArrayList<>();
+            //add route(s) to the map.
+            for (int i = 0; i < route.size(); i++) {
+
+                //In case of more than 5 alternative routes
+                int colorIndex = i % COLORS.length;
+
+                PolylineOptions polyOptions = new PolylineOptions();
+                polyOptions.color(getResources().getColor(COLORS[colorIndex]));
+                polyOptions.width(10 + i * 3);
+                polyOptions.addAll(route.get(i).getPoints());
+                Polyline polyline = googleMap.addPolyline(polyOptions);
+                polylines.add(polyline);
             }
 
-            @Override
-            public View getInfoContents(Marker marker) {
-                // Setting up the infoWindow with current's marker info
-                deleteBtnListener.setMarker(marker);
-                directionBtnListener.setMarker(marker);
-                detailsBtnListener.setMarker(marker);
-                // We must call this to set the current marker and infoWindow references
-                // to the MapWrapperLayout
-                mapWrapperLayout.setMarkerWithInfoWindow(marker, infoWindow);
-                return infoWindow;
-            }
-        });
-    }
 
-
-    public void route(LatLng end) {
-        start = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        this.end = end;
-        if (start == null || end == null) {
-            if (start == null) {
-                showToast("Please choose a starting point.");
-            }
-            if (end == null) {
-                showToast("Please choose a destination.");
-            }
-        } else {
-            showProgressBar();
-            Routing routing = new Routing.Builder()
-                    .travelMode(AbstractRouting.TravelMode.DRIVING)
-                    .withListener(this)
-                    .alternativeRoutes(true)
-                    .waypoints(start, end)
-                    .key(getString(R.string.google_maps_key))
-                    .build();
-            routing.execute();
-        }
-    }
-
-    @Override
-    public void onRoutingFailure(RouteException e) {
-        hideProgressBar();
-        if (e != null && e.getStatusCode() == "ZERO_RESULTS") {
-            showToast("no direction for This location is so far");
-        } else if (e == null) {
-            showToast("Something went wrong, Try again");
-        } else {
-            showToast("Error: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void onRoutingStart() {
-        googleMap.clear();
-        for (Marker marker1 : markers) {
-            googleMap.addMarker(new MarkerOptions().position(marker1.getPosition())
-                    .title(marker1.getTitle()));
-        }
-    }
-
-    @Override
-    public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
-
-        LatLng start = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        hideProgressBar();
-
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(start, 10));
-
-
-        if (polylines.size() > 0) {
-            for (Polyline poly : polylines) {
-                poly.remove();
-            }
-        }
-
-        polylines = new ArrayList<>();
-        //add route(s) to the map.
-        for (int i = 0; i < route.size(); i++) {
-
-            //In case of more than 5 alternative routes
-            int colorIndex = i % COLORS.length;
-
-            PolylineOptions polyOptions = new PolylineOptions();
-            polyOptions.color(getResources().getColor(COLORS[colorIndex]));
-            polyOptions.width(10 + i * 3);
-            polyOptions.addAll(route.get(i).getPoints());
-            Polyline polyline = googleMap.addPolyline(polyOptions);
-            polylines.add(polyline);
         }
 
 
+        @Override
+        public void onRoutingCancelled () {
+            Log.i("routing", "Routing was cancelled.");
+
+        }
     }
-
-
-    @Override
-    public void onRoutingCancelled() {
-        Log.i("routing", "Routing was cancelled.");
-
-    }
-}
